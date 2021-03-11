c3f65f90a9be7aa6bc7d524f6efc9ca29e469e77
Jagadeesh Kedarisetty <jakedari@microsoft.com>
2021-03-10 22:26:59 -0800

    Added cpanel yml file

--- UIApp/src/http-common.js
+++ UIApp/src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: "https://cs411upskillguru.web.illinois.edu/api",
   headers: {
     "Content-type": "application/json"
   }
