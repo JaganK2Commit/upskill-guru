f4661a0de3829199a00a1e33b667cdca20053d24
Jagadeesh Kedarisetty <jakedari@microsoft.com>
2021-03-11 07:11:12 -0800

    Added build files to git

---
+++ UIApp/build/precache-manifest.9319c91d36056a5ce3df947f857c06ce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5558c1ab61deb2f129669194ab106f53",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e4c06581d08f46b4a11f",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "d577ae187aac184b3420",
+    "url": "/static/css/main.7a94a777.chunk.css"
+  },
+  {
+    "revision": "e4c06581d08f46b4a11f",
+    "url": "/static/js/2.160c2e87.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/static/js/2.160c2e87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d577ae187aac184b3420",
+    "url": "/static/js/main.4e0c90c6.chunk.js"
+  },
+  {
+    "revision": "7840a4360155d662a69e",
+    "url": "/static/js/runtime-main.49e9a94d.js"
+  }
+]);
