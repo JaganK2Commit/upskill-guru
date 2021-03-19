abc6fbbf59f7a7c7b6afac0338ad92f1bdea1e16
Denis Stolearenco <deniss3@illinois.edu>
2021-03-19 18:50:29 -0400

    authentication that was moved from model

--- WebAPI/app/controllers/user.controller.js
+++ WebAPI/app/controllers/user.controller.js
@@ -1,19 +1,52 @@
 const db = require("../models");
-const dbUsers = db.users;
-const Op = db.Sequelize.Op;
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 
 // log the user in
-exports.login = (req, res) => {
+exports.login = async (req, res) => {
   const { username, password } = req.query;
-  
-  // console.log(req.query);
 
-  dbUsers.authenticate(username, password)
-    .then(data => { res.send(data)})
-    .catch(err => {
-      console.log("err:" + err);
-      res.status(400).send({
-        message: "Error authenticating user"
-      });
-    });
+  const user = await db.users.findOne({ where: { username } });
+
+  if (!user) {
+    throw new Error('User not found in the database');
+  }
+
+  if (await comparePassword(password, user.dataValues.Password)) {
+    const token = jwt.sign({ sub: user.userId }, 'secret', { expiresIn: '7d' });
+    console.log('success!');
+    // exclude password
+    const { Password, ...userData } = user.get();
+    console.log({ ...userData, token })
+    return { ...omitHash(user.get()), token };
+  }
+
+  throw new Error('invalid password');
 };
+
+
+
+
+function omitHash(user) {
+  const { hash, ...userWithoutHash } = user;
+  return userWithoutHash;
+}
+
+const hashPassword = async(password, saltRounds=10) => {
+  try {
+    const salt = await bcrypt.genSalt(saltRounds);
+    return await bcrypt.hash(password, salt);
+  } catch (err) {
+    console.log(err);
+  }
+  return null;
+}
+
+const comparePassword = async (password, hash) => {
+  try {
+    return await bcrypt.compare(password, hash);
+  } catch (err) {
+    console.log(err);
+  }
+  return false;
+}
