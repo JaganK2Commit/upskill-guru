693124f8f497b20a6acad6dffcbe5bfebd479359
Denis Stolearenco <deniss3@illinois.edu>
2021-03-23 21:16:24 -0400

    .

--- UIApp/src/http-common.js
+++ UIApp/src/http-common.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://api.cs411upskillguru.web.illinois.edu/api",
+  // baseURL: "https://api.cs411upskillguru.web.illinois.edu/api",
   //baseURL: "http://localhost/api",
+  baseURL: "http://localhost:8080/",
   headers: {
     "Content-type": "application/json"
   }
