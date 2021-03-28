cb6ffdd8be452be682d34983423de348b1018689
Denis Stolearenco <deniss3@illinois.edu>
2021-03-27 20:59:24 -0400

    added use context provider

--- UIApp/src/App.js
+++ UIApp/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useMemo, useEffect } from "react";
 import { Switch, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
@@ -9,7 +9,6 @@ import styles from './App.style';
 import AddSkill from "./components/AddSkill";
 import Skill from "./components/Skill";
 import SkillsList from "./components/SkillsList";
-import Login from "./components/Login/Login"
 import LeftNav from "./components/LeftNav/LeftNav";
 import Account from "./components/Account/Account";
 import SavedSearch from "./components/SavedSearch/SavedSearch";
@@ -18,64 +17,72 @@ import Front from "./components/Front/Front";
 import CreateAccount from "./components/CreateAccount/CreateAccount";
 import Login from "./components/Login/Login";
 import ManageDB from "./components/ManageDB/ManageDB";
+
+import { UserContext } from './UserContext';
+
 function App() {
-  const [loggedIn, setLoggedIn] = useState(localStorage.getItem('token'));
-  initializeIcons();
+  const [user, setUser] = useState(null);
+  const value = useMemo(() => ({ user, setUser }), [user, setUser]);
+
+  useEffect(() => {
+    setUser(localStorage.getItem('token'));
+  });
+  // initializeIcons();
 
   const logout = () => {
     localStorage.removeItem('token');
-    setLoggedIn(false);
+    setUser(null);
   }
   
   return (
-    <div>
-      <Stack vertical className={styles.Container}>
-        <Stack>
-          <nav className="navbar navbar-expand navbar-dark bg-dark">
-            <a href="/" className="navbar-brand">
-              UpSkill-Guru
-            </a>
-            <div align='end'>
-            { !loggedIn 
-              ? <Link href="/login">login</Link>
-              : <Link href="#" onClick={() => logout()}>logout</Link>
-            }
-            </div>
-          </nav>
-        </Stack>
-        <Stack>
-          <Stack horizontal>
-            <Stack>
-                <LeftNav>
-                </LeftNav>
-            </Stack>
-
-            <Stack
-              className={styles.mainContentContainer}
-            >
-
-            <div>
-            <Switch>
-              <Route exact path={["/", "/skills"]} component={SkillsList} />
-              <Route exact path={["/Account"]} component={Account} />
-              <Route exact path={["/SavedSearch"]} component={SavedSearch} />
-              <Route exact path={["/CreateAccount"]} component={CreateAccount} />
-              <Route exact path={["/Login"]} component={Login} />
-              <Route exact path={["/SearchResult"]} component={SearchResult} />
-              <Route exact path={["/Front"]} component={Front} />
-              <Route exact path={["/ManageDB"]} component={ManageDB} />
-              <Route exact path="/add" component={AddSkill} />
-              <Route path="/skills/:id" component={Skill} />
-              <Route path="/login" component={Login} />
-            </Switch>
-            </div>
-            </Stack>
-            </Stack>
+    <UserContext.Provider value={value}>
+      <div>
+        <Stack vertical className={styles.Container}>
+          <Stack>
+            <nav className="navbar navbar-expand navbar-dark bg-dark">
+              <a href="/" className="navbar-brand">
+                UpSkill-Guru
+              </a>
+              <div align='end'>
+              { !user 
+                ? <Link href="/login">login</Link>
+                : <Link href="#" onClick={() => logout()}>logout</Link>
+              }
+              </div>
+            </nav>
           </Stack>
-      </Stack>
+          <Stack>
+            <Stack horizontal>
+              <Stack>
+                  <LeftNav>
+                  </LeftNav>
+              </Stack>
 
-      
-    </div>
+              <Stack
+                className={styles.mainContentContainer}
+              >
+
+              <div>
+              <Switch>
+                <Route exact path={["/", "/skills"]} component={SkillsList} />
+                <Route exact path={["/Account"]} component={Account} />
+                <Route exact path={["/SavedSearch"]} component={SavedSearch} />
+                <Route exact path={["/CreateAccount"]} component={CreateAccount} />
+                <Route exact path={["/Login"]} component={Login} />
+                <Route exact path={["/SearchResult"]} component={SearchResult} />
+                <Route exact path={["/Front"]} component={Front} />
+                <Route exact path={["/ManageDB"]} component={ManageDB} />
+                <Route exact path="/add" component={AddSkill} />
+                <Route path="/skills/:id" component={Skill} />
+                <Route path="/login" component={Login} />
+              </Switch>
+              </div>
+              </Stack>
+              </Stack>
+            </Stack>
+        </Stack>       
+      </div>
+    </UserContext.Provider>
   );
 }
 
