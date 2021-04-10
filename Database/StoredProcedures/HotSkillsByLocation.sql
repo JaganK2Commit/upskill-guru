3e76cce4cf67ece47aff57c4a66725c124835d1a
Denis Stolearenco <deniss3@illinois.edu>
2021-04-09 20:40:53 -0400

    .

--- Database/StoredProcedures/HotSkillsByLocation.sql
+++ Database/StoredProcedures/HotSkillsByLocation.sql
@@ -3,15 +3,15 @@ CREATE DEFINER=`root`@`localhost` PROCEDURE `HotSkillsByLocation`(
     IN `jobTitleKeyword` VARCHAR(255)
 )
 BEGIN
-SELECT j.JobTitle, s.SkillName AS skill_name, CONCAT_WS(',', l.City, l.State) AS joblocation_address,l.Latitude AS lat,l.Longitude AS longi, COUNT(l.city) AS frequency
+SELECT * FROM (SELECT j.JobTitle, s.SkillName AS skill_name, CONCAT_WS(',', l.City, l.State) AS joblocation_address,l.Latitude AS lat,l.Longitude AS longi, COUNT(l.city) AS frequency
 FROM jobs j
 INNER JOIN jobskills js ON js.JobId= j.JobId
-INNER JOIN skills s ON js.SkillId = s.SkillId 
-INNER JOIN joblocation jl ON js.JobId = jl.LocationId
+INNER JOIN skills s ON js.SkillId = s.SkillId
+INNER JOIN joblocation jl ON js.JobId = jl.JobId
 INNER JOIN locations l ON jl.LocationId = l.LocationId
-WHERE lower(j.JobTitle) LIKE CONCAT('%', lower(jobTitleKeyword) , '%')
-GROUP BY s.SkillName, l.City, l.State,l.State,l.Latitude,l.Longitude
-ORDER BY  COUNT(l.city) DESC
+GROUP BY s.SkillName
+ORDER BY  COUNT(l.city) DESC)tmp
+WHERE lower(tmp.JobTitle) LIKE CONCAT('%', lower(jobTitleKeyword), '%')
 LIMIT top;
 
 END
