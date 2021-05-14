76a8f3055958fc482304bf33e3f901760a3b2037
Jagadeesh Kedarisetty <jakedari@microsoft.com>
2021-05-14 10:56:53 -0700

    Updated relevantSkillsSet

--- Database/StoredProcedures/getRelevantSkillSets.sql
+++ Database/StoredProcedures/getRelevantSkillSets.sql
@@ -4,19 +4,19 @@
 -- Server OS:                    Win64
 -- HeidiSQL Version:             11.2.0.6213
 -- --------------------------------------------------------
-
+​
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET NAMES utf8 */;
 /*!50503 SET NAMES utf8mb4 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
-
+​
+​
 -- Dumping database structure for upskillguru
 CREATE DATABASE IF NOT EXISTS `upskillguru` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE `upskillguru`;
-
+​
 -- Dumping structure for procedure upskillguru.getRelevantSkillSet
 DELIMITER //
 CREATE PROCEDURE `getRelevantSkillSet`(
@@ -25,16 +25,41 @@ CREATE PROCEDURE `getRelevantSkillSet`(
 	IN `userId` INT
 )
 BEGIN
-  SELECT skillSet, SUBSTRING(skillSet, 1, 50) AS substringtitle, count(1) AS Frequency FROM relevantskillssets rs
-LEFT OUTER JOIN (Select SkillId from userskills us where UserId = userId) as us ON us.SkillId = rs.SkillId
-WHERE LOWER(rs.jobtitle) LIKE CONCAT('%', lower(jobTitleKeyword), '%')
-GROUP BY skillset
-ORDER BY Frequency DESC
-LIMIT top;
+   
+​
+     with CTE_JobsHavingAMatch AS (
+			SELECT js.JobId as JobId,s.SkillId as JobSkillId, s.SkillName as SkillName, us.SkillId as UserSkillId FROM
+			jobs j
+			JOIN jobskills js ON j.JobId = js.JobId
+			JOIN skills s ON s.SkillId = js.SkillId
+			LEFT JOIN (Select SkillId from userskills us where UserId = userId) as us ON us.SkillId = js.SkillId
+			WHERE LOWER(j.JobTitle) LIKE CONCAT('%', lower(jobTitleKeyword), '%'))
+			
+			SELECT Substring(SkillsToLearn, 1, 75) AS skillSet,COUNT(SkillsToLearn) AS Frequency FROM (
+			SELECT jo.JobId, GROUP_CONCAT( jo.SkillName) as SkillsToLearn,
+			COUNT(jo.SkillName) as SkillsToLearnCount,
+			FLOOR(SUM(ji.SkillMatchCount)/COUNT(jo.JobSkillId)) as SkillMatchCount
+			FROM CTE_JobsHavingAMatch jo
+			JOIN (
+				SELECT JobId, COUNT(UserSkillId) as SkillMatchCount FROM CTE_JobsHavingAMatch
+			    WHERE UserSkillId is not NULL
+			    GROUP BY JobId
+				) as ji
+			ON jo.JobId = ji.JobId
+			 WHERe jo.UserSkillId is NULL
+			GROUP BY jo.JobId
+			ORDER BY SkillsToLearnCount ASC, SkillMatchCount DESC
+			) AS relevantJobs
+			GROUP BY SkillsToLearn
+			ORDER BY COUNT(SkillsToLearn) DESC
+			LIMIT top; 
+ 
 END//
 DELIMITER ;
-
+​
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
+Collapse
+
